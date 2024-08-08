0c9db3d4035a3f6def38b80b8c9c44662eb617b8
Bartek <baarti02@gmail.com>
2024-08-08 12:34:28 +0200

    Nowy plik do drzewa binarnego

---
+++ Drzewa/BinaryTree.go
@@ -0,0 +1,14 @@
+package Drzewa
+
+import (
+	"fmt"
+)
+
+func BinaryTree() {
+	var intArr [3]int16 = [3]int16{1, 2, 3}
+	fmt.Println(intArr)
+	var myMap map[string]uint8 = make(map[string]uint8)
+	myMap["Adam"] = 30
+	myMap["Bartek"] = 22
+	fmt.Println(myMap)
+}
--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
-
-	animals "github.com/barphantom/AlgorytmyGolang/Animals"
-	listy "github.com/barphantom/AlgorytmyGolang/Listy"
+	// "fmt"
+	// animals "github.com/barphantom/AlgorytmyGolang/Animals"
+	// listy "github.com/barphantom/AlgorytmyGolang/Listy"
+	drzewa "github.com/barphantom/AlgorytmyGolang/Drzewa"
 )
 
 func main() {
-	fmt.Println("Hello")
-	animals.Mew()
-	animals.Bark()
-	listy.StartFifoQueue()
+	// fmt.Println("Hello")
+	// animals.Mew()
+	// animals.Bark()
+	// listy.StartFifoQueue()
+	drzewa.BinaryTree()
 }
